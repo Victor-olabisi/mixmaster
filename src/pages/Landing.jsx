2759d9f7471e2380a5744610261c1360b97d0b61
victor <oguntayovictor65@gmail.com>
2023-11-06 08:57:05 -0800

    added cocktail details component

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,6 @@
 import { useLoaderData } from "react-router-dom"
 import axios from "axios"
+import CocktailList from "../component/CocktailList";
 
 const cocktailSearchUrl =
   'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
@@ -13,8 +14,11 @@ export const loader = async () => {
 const Landing = () => {
     const { drinks, searchTerm } = useLoaderData();
     console.log(drinks);
-  return (
-    <div>Landing</div>
+    return (
+        <>
+            <CocktailList drinks={drinks} />
+            
+        </>
   )
 }
 export default Landing
